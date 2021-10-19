190df24ef8da9b2da477ea7322abebdf5fa303e7
Aayushma Gautam <gtmaayushma@gmail.com>
2021-10-19 11:00:06 -0400

    some collision stuff- didnt work fully

--- UberRush/Assets/MoveCar.cs
+++ UberRush/Assets/MoveCar.cs
@@ -22,11 +22,62 @@ public class MoveCar : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        vertical = Input.GetAxisRaw("Vertical"); 
     }
 
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Passenger"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+
+//public class MoveCar : MonoBehaviour
+//{
+//    public float speed = 0;
+
+//    private Rigidbody2D rb;
+
+//    private float movementX;
+//    private float movementY;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        rb = GetComponent<Rigidbody2D>();
+//    }
+
+//    private void OnMove(InputValue movementValue)
+//    {
+//        Vector2 movementVector = movementValue.Get<Vector2>();
+
+//        movementX = movementVector.x;
+//        movementY = movementVector.y;
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+//        rb.AddForce(movement * speed);
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Passenger"))
+//        {
+//            other.gameObject.SetActive(false);
+//        }
+//    }
+//}
+
