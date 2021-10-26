9a48051cbec93bb999dff2e0c0438326ea301b13
dalayoub <92409520+dalayoub@users.noreply.github.com>
2021-10-25 21:01:57 -0400

    jack's work (3 moving taxis as obstacles)

---
+++ UberRush/Assets/NPC_PatrolRandomPoints.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class NPC_PatrolRandomPoints : MonoBehaviour {
+
+       public float speed = 10f;
+       private float waitTime;
+       public float startWaitTime = 2f;
+
+       public Transform[] moveSpots;
+       private int randomSpot;
+
+       void Start(){
+              waitTime = startWaitTime;
+              randomSpot = Random.Range(0, moveSpots.Length);
+       }
+
+       void Update(){
+              transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+              if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
+                     if (waitTime <= 0){
+                            randomSpot = Random.Range(0, moveSpots.Length);
+                            waitTime = startWaitTime;
+                     } else {
+                            waitTime -= Time.deltaTime;
+                     }
+              }
+       }
+}
